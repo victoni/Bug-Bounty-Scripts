0ea94d9ef71404bee98ad2020421fd14f61a2c9a
vict0ni <vicmarkopoulos@gmail.com>
2020-06-05 12:45:07 +0200

    update lazy

--- googleDorks.md
+++ googleDorks.md
@@ -9,3 +9,10 @@ Amazon S3 Buckets:
 Open Redirections/SSRF:
 
 `site:domain.com inrul:[PARAMETER]` using the [parameter list](https://github.com/victoni/Bug-Bounty-Scripts/blob/master/open_redirection_parameters.txt)
+
+Subdomain discovery:
+
+`site:domain.com -site:www.domain.com`
+`site:domain.com -inurl:www`
+`site:domain.com -www`
+`site:*.domain.com`
--- lazy.sh
+++ lazy.sh
@@ -4,9 +4,7 @@ subfinder -d $1 | tee -a domains
 assetfinder -subs-only $1 | tee -a domains
 crobat-client -s $1 | tee -a domains
 curl -s "https://crt.sh/\?q\=%25.$1\&output\=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a domains
-#subbrute/subbrute.py -r subbrute/resolvers.txt -o subb.txt -s subbrute/names.txt $1
-
-echo "[*] Subdomain discovery ended"
+cat /home/victor/Desktop/bugHunting/resources/wordlists/dns-Jhaddix.txt | subgen -d "$1" | zdns A | jq -r "select(.data.answers[0].name) | .name" | tee -a domains
 
 #sorting/uniq
 #cat subb.txt >> domains
@@ -14,27 +12,26 @@ sort -u domains > dom2;rm domains;mv dom2 domains
 
 #account takeover scanning
 subjack -w domains -t 100 -timeout 30 -ssl -c /home/victor/go/src/github.com/haccer/subjack/fingerprints.json -v | tee -a takeover
-echo "[*] Subdomain takeover check ended"
 
-#httprobing and endpoint discovery from weybackurls 
+#httprobing 
 cat domains | httprobe | tee -a responsive
-echo "[*] httprobe ended"
+gf interestingsubs responsive > interestingsubs
 
-cat responsive | waybackurls | tee -a all_urls
-echo "[*] waybackurls ended"
+#endpoint discovery
+cat responsive | gau | tee -a all_urls
+cat responsive | hakrawler --depth 3 --plain | tee -a all_urls
 
 #extracting all responsive js files
 grep "\.js$" all_urls | anti-burl | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | sort -u | tee -a javascript_files
-echo "[*] collecting all js files ended"
 
 #analyzing js files for secrets
-mkdir js;cd js;/home/victor/Desktop/bugHunting/tools/Bug-Bounty-Scripts/wgetlist ../javascript_files; cd ..
-echo "[*] analyzing js files ended"
+mkdir js;cd js;/home/victor/Desktop/bugHunting/tools/Bug-Bounty-Scripts/wgetlist ../javascript_files; cat * >> GF; gf sec GF > gf_sec; rm GF; > sec; cd ..
 
-#grabing endpoints that include parameters which point to internal paths or external endpoints
+#grabing endpoints that include juicy parameters
 gf redirect all_urls | anti-burl > redirects
-echo "[*] greping possible redirects ended"
-
-#automating xss
-#/home/victor/Desktop/bugHunting/tools/Bug-Bounty-Scripts/autoxss domains
-#echo "[*] automating xss ended"
+gf idor all_urls | anti-burl > idor
+gf rce all_urls | anti-burl > rce
+gf lfi all_urls | anti-burl > lfi
+gf xss all_urls | anti-burl > xss
+gf xss all_urls | anti-burl > xss
+gf ssrf all_urls | anti-burl > ssrf
