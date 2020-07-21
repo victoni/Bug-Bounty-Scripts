4b17b5ea72713956dc0ff121547f9c2e86db3b09
vict0ni <vicmarkopoulos@gmail.com>
2020-07-21 17:59:24 +0200

    modified jcollect and added it to lazy

--- lazy.sh
+++ lazy.sh
@@ -1,6 +1,8 @@
-
 #finding subdomains
+
 #cat /home/victor/Desktop/bugHunting/resources/wordlists/dns-Jhaddix.txt | subgen -d "$1" | zdns A | jq -r "select(.data.answers[0].name) | .name" | tee -a domains
+# amass enum -v -src -ip -brute -min-for-recursive 2 -d $1
+# amass enum -active -d owasp.org -public-dns -brute -w /root/dns_lists/deepmagic.com-top50kprefixes.txt -src -ip -dir amass4owasp -config /root/amass/config.ini -o amass_results_owasp.txt
 subfinder -d $1 | tee -a domains
 assetfinder -subs-only $1 | tee -a domains
 crobat-client -s $1 | tee -a domains
@@ -15,6 +17,8 @@ curl -s "https://rapiddns.io/subdomain/$1?full=1" \
 
 rapiddns $1 | tee -a domains
 
+
+
 #sorting/uniq
 #cat subb.txt >> domains
 sort -u domains > dom2;rm domains;mv dom2 domains
@@ -27,20 +31,23 @@ cat domains | httprobe | tee -a responsive
 gf interestingsubs responsive > interestingsubs
 
 #endpoint discovery
-cat responsive | gau | tee -a all_urls
+cat responsive | gau -subs | tee -a all_urls
 cat responsive | hakrawler --depth 3 --plain | tee -a all_urls
+
 
 #extracting all responsive js files
 grep "\.js$" all_urls | anti-burl | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | sort -u | tee -a javascript_files
 
-#analyzing js files for secrets
-mkdir js;cd js;/home/victor/Desktop/bugHunting/tools/Bug-Bounty-Scripts/wgetlist ../javascript_files
+
+while read LINE; do 
+	python3 /home/victor/Desktop/bugHunting/tools/secretfinder/SecretFinder.py -i $LINE -o cli | tee -a js_results
+done < javascript_files
+
 
 #grabing endpoints that include juicy parameters
 gf redirect all_urls | anti-burl > redirects
 gf idor all_urls | anti-burl > idor
 gf rce all_urls | anti-burl > rce
 gf lfi all_urls | anti-burl > lfi
-gf xss all_urls | anti-burl > xss
 gf xss all_urls | anti-burl > xss
 gf ssrf all_urls | anti-burl > ssrf
