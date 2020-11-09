0e7bd97f8837edc88523c20df70483e612ac3e3a
vict0ni <32034171+victoni@users.noreply.github.com>
2020-11-09 17:33:57 +0100

    doc

--- crtOrg.sh
+++ crtOrg.sh
@@ -1,1 +1,5 @@
+# Get subdomains from crt.sh based on Org's name (Subject Name Organization)
+# bash crtOrg.sh [Org's name]
+# e.g.
+# bash crtOrg.sh Red Bull GmbH
 curl -s "https://crt.sh/?O=$(echo $@ | sed -e 's/\ /+/g')\&output=json" | jq -r .[].common_name | grep -v null | grep -v " " | sort -u
