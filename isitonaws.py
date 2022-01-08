b2f66ec3502a10486ae163590496b77ce519cebd
Viktor Markopoulos <vicmarkopoulos@gmail.com>
2022-01-08 19:29:46 +0200

    update isinonaws for urls

--- isitonaws.py
+++ isitonaws.py
@@ -1,11 +1,12 @@
-# CLI tool of https://isitonaws.com/
+# CLI tool for https://isitonaws.com/ (except ipv6 addresses, they're ugly)
 
 from ipaddress import ip_network, ip_address
 from sys import argv
 from requests import get
 from json import loads, dumps
+from socket import gethostbyname
 
-ip = ip_address(str(argv[1]))
+ip = ip_address(gethostbyname(str(argv[1])))
 ranges = loads(get('https://ip-ranges.amazonaws.com/ip-ranges.json').text)
 
 for cidr in ranges['prefixes']:
